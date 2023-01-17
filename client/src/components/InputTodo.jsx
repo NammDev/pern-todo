a75fa4e8f36490163a56cc8ddaa023e6665b1333
Nam <namdeveloper.ca@gmail.com>
2023-01-17 16:48:31 +0700

    Axios & submit form to add todo

---
+++ client/src/components/InputTodo.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import axios from '../utils/axiosCustom'
+
+function InputTodo() {
+  const [description, setDescription] = useState(' ')
+
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const body = { description }
+      const response = await axios.post('/todos', JSON.stringify(body))
+      console.log(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <div className='container'>
+      <h1 className='text-center my-5'>Pern Todo List</h1>
+      <form className='d-flex' onSubmit={onSubmit}>
+        <input
+          type='text'
+          className='form-control'
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <button className='btn btn-success'>Add</button>
+      </form>
+    </div>
+  )
+}
+
+export default InputTodo
