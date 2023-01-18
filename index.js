5b83fe5d0414c676f788e273e79823f30071b834
Nam <namdeveloper.ca@gmail.com>
2023-01-18 10:29:14 +0700

    ready for deployment

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ const PORT = process.env.PORT || 3000
 app.use(cors())
 app.use(express.json())
 
+app.use(express.static(path.join(__dirname, 'client/build')))
 if (process.env.NODE_ENV === 'production') {
   // serve static content
-  app.use(express.static(path.join(__dirname, 'client/build')))
 }
 
 // ROUTES
